56856aec1bfefc5bc8c3f4c4cc5d0c2ee81b3ae3
ɐllɐ <molchanova.a.a@gmail.com>
2024-08-29 13:33:51 +0300

    task 4.1.4

---
+++ ConsoleApp4CLR/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string a = "";
+        string b = "4";
+
+        bool c = a != b;
+        Console.WriteLine(c);
+        Console.ReadKey();
+
+
+    }
+}
