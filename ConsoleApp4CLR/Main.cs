417e73cf848225f58e70191891fecbfb43b282c2
ɐllɐ <molchanova.a.a@gmail.com>
2024-08-29 13:52:23 +0300

    task 4.1.5

--- ConsoleApp4CLR/Main.cs
+++ ConsoleApp4CLR/Main.cs
@@ -4,10 +4,23 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        string a = "";
-        string b = "4";
+        /*
+         переменная A типа int меньше переменной B типа int,
+        или переменная X типа double больше переменной Y типа double.
 
-        bool c = a != b;
+Внимание: для начала необходимо объявить переменные.
+        Значение выражения положить в переменную C логического типа.
+        Задавать значения для A, B, X и Y не нужно.
+         */
+
+
+        int a;
+        int b;
+        double x;
+        double y;
+
+
+        bool c = (a < b) | (x > y);
         Console.WriteLine(c);
         Console.ReadKey();
 
