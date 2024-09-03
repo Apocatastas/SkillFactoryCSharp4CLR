4e9e64e7887ab0ed6aa8e87aea4d9d8631b80a0f
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-03 22:09:20 +0300

    task 4.3.15

--- ConsoleApp4CLR/Main.cs
+++ ConsoleApp4CLR/Main.cs
@@ -4,20 +4,22 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[][] array = new int[3][];
+        int positiveQuantity = 0;
+        var arr = new int[15];
 
-        array[0] = new int[2] { 1, 2 };
-        array[1] = new int[3] { 1, 2, 3 };
-        array[2] = new int[5] { 1, 2, 3, 4, 5 };
-
-        foreach (var num in array)
+        for (int i = 0; i < arr.Length; i++)
         {
-            foreach (var item in num)
-            {
-                Console.Write(item + " ");
-            }
-            Console.Write("\n");
+            Console.Write("Please enter element number {0}: ", i + 1);
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            if (arr[i] > 0) { positiveQuantity++; };
         }
+        Console.Write("\nARRAY: ");
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\nPositive quantity: " + positiveQuantity);
+    
         Console.ReadKey();
     }
 
