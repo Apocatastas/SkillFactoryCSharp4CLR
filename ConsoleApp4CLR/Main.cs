aa4d92ede14eb1e6b956f0991f0c61fccb5d5914
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-09 21:33:46 +0300

    task 4.5.6

--- ConsoleApp4CLR/Main.cs
+++ ConsoleApp4CLR/Main.cs
@@ -14,37 +14,40 @@ class MainClass
 
         (string FirstName, string LastName, string Login, int LoginLength, bool IsHasPet, int Age, string[] FavColors ) User;
 
-        Console.Write("Введите имя: ");
-        User.FirstName = Console.ReadLine();
-        Console.Write("Введите фамилию: ");
-        User.LastName = Console.ReadLine();
-        Console.Write("Введите логин: ");
-        User.Login = Console.ReadLine();
-        User.LoginLength = User.Login.Length;
-        Console.Write("Есть ли у вас животные (Да/Нет): ");
-        var pets = Console.ReadLine();
-        if (Convert.ToString(pets) == "Да")
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Введите имя: ");
+            User.FirstName = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            User.LastName = Console.ReadLine();
+            Console.Write("Введите логин: ");
+            User.Login = Console.ReadLine();
+            User.LoginLength = User.Login.Length;
+            Console.Write("Есть ли у вас животные (Да/Нет): ");
+            var pets = Console.ReadLine();
+            if (Convert.ToString(pets) == "Да")
             {
-            User.IsHasPet = true;
+                User.IsHasPet = true;
             }
 
-        else
+            else
             {
-            User.IsHasPet = false;
+                User.IsHasPet = false;
             }
 
-        Console.Write("Введите возраст: ");
-        User.Age = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Введите ваши любимые три цвета\n");
-        User.FavColors = new string[3];
-        Console.Write("#1: ");
-        User.FavColors[0] = Console.ReadLine();
-        Console.Write("#2: ");
-        User.FavColors[1] = Console.ReadLine();
-        Console.Write("#3: ");
-        User.FavColors[2] = Console.ReadLine();
-
-
+            Console.Write("Введите возраст: ");
+            User.Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваши любимые три цвета\n");
+            User.FavColors = new string[3];
+            Console.Write("#1: ");
+            User.FavColors[0] = Console.ReadLine();
+            Console.Write("#2: ");
+            User.FavColors[1] = Console.ReadLine();
+            Console.Write("#3: ");
+            User.FavColors[2] = Console.ReadLine();
+        }
+       
 
         Console.ReadKey();
 
