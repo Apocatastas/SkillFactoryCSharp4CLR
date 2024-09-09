166d8d567c32d4254df3dabd17c098085e6a47cf
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-09 21:14:34 +0300

    task 4.5.1

--- ConsoleApp4CLR/Main.cs
+++ ConsoleApp4CLR/Main.cs
@@ -3,42 +3,18 @@ using System.Xml.Linq;
 
 class MainClass
 {
-    enum PetType 
-    {
-        кошка,
-        рыбка,
-        хомячок
-    }
-
-
     public static void Main(string[] args)
     {
-
-        (string Name, string Type, double Age, int NameCount) Pet;
-
-        Console.Write("Введите имя: ");
-        Pet.Name = Console.ReadLine();
-
-
-        do
-        {
-            Console.Write("Введите тип (кошка, рыбка или хомячок): ");
-            Pet.Type = Console.ReadLine();
-        }
-        while
-           ((Convert.ToString(Pet.Type) != Convert.ToString(PetType.кошка)) &&
-            (Convert.ToString(Pet.Type) != Convert.ToString(PetType.рыбка)) &&
-            (Convert.ToString(Pet.Type) != Convert.ToString(PetType.хомячок)));
+       //     Необходимо разработать методику анкетирования пользователей.
+       //     Требуется сохранять информацию: об имени пользователя, фамилии,
+       //     логине, длине логина, наличии/ отсутствии у пользователя питомца,
+       //     возрасте пользователя, трех любимых цветах пользователя.
 
 
-        Console.Write("Введите возраст с цифрами: ");
-        Pet.Age = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Имя питомца: {0}", Pet.Name);
-        Console.WriteLine("Тип питомца: {0}", Pet.Type);
-        Console.WriteLine("Возраст питомца: {0}", Pet.Age);
-        Console.WriteLine("Длина имени питомца: {0}", Pet.Name.Length);
+        (string FirstName, string LastName, string Login, int LoginLength, bool IsHasPet, int Age, int[] FavColors ) User;
 
+       
         Console.ReadKey();
 
     }
