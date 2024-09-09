4bca9c7436f7f293671c000c49acd03a060e07c9
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-09 20:39:40 +0300

    task 4.4.3

--- ConsoleApp4CLR/Main.cs
+++ ConsoleApp4CLR/Main.cs
@@ -4,15 +4,17 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        (string name, int age) anketa;
+        var (name, age) = ("Satan", 666);
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
 
         Console.Write("Введите имя: ");
-        anketa.name = Console.ReadLine();
+        name = Console.ReadLine();
         Console.Write("Введите возраст с цифрами: ");
-        anketa.age = Convert.ToInt32(Console.ReadLine());
+        age = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Ваше имя: {0}", anketa.name);
-        Console.WriteLine("Ваш возраст: {0}", anketa.age);
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
 
         Console.ReadKey();
 
